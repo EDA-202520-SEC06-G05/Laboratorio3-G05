35c89f77365aee279ce800239bab9691d51fafdb
San-FG <manuelsal5436-gmail.com>
2025-08-20 15:12:23 -0500

    Cambios en single linked list

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -1,8 +1,20 @@
-
 def new_list():
     newlist = {
         "first": None,
         "last": None,
         "size": 0,
     }
-    return new_list
+    return newlist
+
+
+def get_element(my_list, pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos :
+        node = node["next"]
+        searchpos += 1
+    return node["info"] 
+
+def is_present(my_list, element, cmp_function):
+    is_in_array = False
+    temp
